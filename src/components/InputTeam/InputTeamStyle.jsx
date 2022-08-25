53cc2aeaba29b903c13fdd066591e85dce55f0a4
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-25 11:07:33 -0300

    style: change input background and space and text and input

--- src/components/InputTeam/InputTeamStyle.jsx
+++ src/components/InputTeam/InputTeamStyle.jsx
@@ -4,12 +4,15 @@ export const InputStyled = styled.div`
     display: flex;
     margin: 10px 0;
     align-items: center;
-
+    justify-content: space-between;
+    padding-right: 10px;
 
     & input{
-        margin-left: 6px;
-        width: 100%;
+        width: 94%;
         padding: 6px;
         border-radius: 4px;
+        margin-left: 6px;
+        background-color: var(--white);
+        border: solid black 2px;
     }
 `
