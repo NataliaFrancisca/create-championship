47b9d227e0450cb5589ef18d4095ba51b5eeddc0
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-25 16:11:22 -0300

    style: create title

---
+++ src/pages/Matches/MatchesStyle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MatchesStyled = styled.main`
+    
+    & h1{
+        font-family: var(--title);
+        font-size: 2em;
+        padding: 10px;
+    }
+`
