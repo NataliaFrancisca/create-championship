ae6a162bb124aedf449f3e6575eaad2cb4b4ca04
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-31 14:57:03 -0300

    feat: use input radio at form, than user can define who win the match

--- src/components/Match/Match.jsx
+++ src/components/Match/Match.jsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
-
 import { MatchStyled } from "./MatchStyle";
+import { useTeamsContext } from "../../hook/useTeamsContext";
 
 const Match = ({matchData, numberMatch}) => {
 
     const [firstTeam, setFirstTeam] = useState();
     const [secondTeam, setSecondTeam] = useState();
-    const [winner, setWinner] = useState({first_team: false, second_team: false});
+    const {winners, setWinners} = useTeamsContext();
+
+    const matchNumber = numberMatch + 1;
 
     const getDataFromTeam = () => {
         const data = JSON.parse(localStorage.getItem("teamNumber"));
         const firstTeam = data.find(team => team.id == matchData[0]);
         const secondTeam = data.find(team => team.id == matchData[1]);
 
+        console.log(firstTeam)
+        console.log(secondTeam)
+
         setFirstTeam(firstTeam)
         setSecondTeam(secondTeam);
+    }
+
+    const setWinnerFromMatch = (value) => {
+        const winnerMatch = {numberMatch: matchNumber, winner: value.nameTeam};
+        const filteredArr = winners.filter(match => match.numberMatch !== winnerMatch.numberMatch);
+        setWinners([...filteredArr, winnerMatch]);
+        console.log(winners)
     }
 
     useEffect(() => {
@@ -41,21 +53,35 @@ const Match = ({matchData, numberMatch}) => {
         }
     },[secondTeam])
 
-
     return(
         <MatchStyled>
-            <h1>#{numberMatch + 1}</h1>
-            <div id="container-dashboard">
-                <span class="team"
-                    onClick={() => setWinner({first_team: true, second_team: false})}>
-                    {firstTeam?.nameTeam}
-                </span>
+            <h1>#{matchNumber}</h1>
+                <form id="container-dashboard">
+                    <div>
+                        <input 
+                            type="radio" 
+                            id={`first_team_${matchNumber}`} 
+                            name={`match_${matchNumber}`} 
+                            value={firstTeam?.nameTeam} 
+                            onChange={() => setWinnerFromMatch(firstTeam)}
+                        />
+                        <label className="team" for={`first_team_${matchNumber}`}>{firstTeam?.nameTeam}</label>
+                    </div>
+
                     <span id="winner">X</span>
-                <span class="team" 
-                    onClick={() => setWinner({first_team: false, second_team: true})}>
-                    {secondTeam?.nameTeam}
-                </span>
-            </div>
+
+                    <div>
+                        <input 
+                            type="radio" 
+                            id={`second_team_${matchNumber}`} 
+                            name={`match_${matchNumber}`} 
+                            value={secondTeam?.nameTeam} 
+                            onChange={() => setWinnerFromMatch(secondTeam)}
+                        />
+                        <label className="team" for={`second_team_${matchNumber}`}>{secondTeam?.nameTeam}</label>
+                    </div>
+                </form>
+               
         </MatchStyled>
     )
 }
